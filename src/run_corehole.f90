bee940a3424f9a285738b2abcb3d848c725cb32a
John Vinson <jtv3@uw.edu>
2023-12-13 10:04:30 -0500

    Option to use frozen valence density when determining core-hole potential surfaced to main program, but still hard coded for now

--- src/run_corehole.f90
+++ src/run_corehole.f90
@@ -16,7 +16,7 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !
- subroutine run_corehole( rr,zz,mmax,srel,nc,nv,na,la,ea,fa,iexc )
+ subroutine run_corehole( rr,zz,mmax,srel,nc,nv,na,la,ea,fa,iexc, keep_valence )
 
 ! computes the core-hole potential, screened by the relaxation of the other core electrons
 ! TODO: mimic Shirley's atomic code options for 
@@ -32,6 +32,7 @@
 !ea energies
 !fa occupations
 !iexc exchange-correlation potential
+!keep_valence flag sets if valence density is included in the calculation of the core relaxation
 
  implicit none
  integer, parameter :: dp=kind(1.0d0)
@@ -47,6 +48,7 @@
  real(dp),intent(in) :: ea(nc+nv)
  real(dp),intent(in) :: fa(nc+nv)
  integer, intent(in) :: iexc
+ logical,intent(in) :: keep_valence
 
 
  integer :: ii, ierr, it, jj
@@ -59,31 +61,37 @@
  allocate( vfull( mmax ), rhoc( mmax ), rho(mmax ), vo(mmax), vo2(mmax), rhoval(mmax) )
 
  tmp_fa(:) = 0.0_DP
- call sratom(na,la,ea,fa,rpk,nc,nc+nv,it,rhoc,rho, &
-&            rr,vfull,zz,mmax,iexc,etot,ierr,srel)
- rhoval(:) = rho(:) - rhoc(:)
- open(unit=99,file='derp.txt')
- do ii = 1, mmax
-   write(99,'(4E24.12)') rr(ii), rho(ii), rhoc(ii), rhoval(ii)
- enddo
- close(99)
- if( nv .eq. 0 ) then
-  sf_in = 0.0_DP
+
+ if( keep_valence) then
+   call sratom(na,la,ea,fa,rpk,nc,nc+nv,it,rhoc,rho, &
+  &            rr,vfull,zz,mmax,iexc,etot,ierr,srel)
+   rhoval(:) = rho(:) - rhoc(:)
+!#if DEBUG
+!   open(unit=99,file='derp.txt')
+!   do ii = 1, mmax
+!     write(99,'(4E24.12)') rr(ii), rho(ii), rhoc(ii), rhoval(ii)
+!   enddo
+!   close(99)
+!#endif
+   if( nv .eq. 0 ) then
+    sf_in = 0.0_DP
+   else
+     sf_in = sum( fa(nc+1:nc+nv) )
+   endif
  else
-   sf_in = sum( fa(nc+1:nc+nv) )
+   rhoval(:) = 0.0_DP
+   sf_in = 0.0_DP
  endif
 
  sume = sum(fa(:))
  do ii = 1, nc
    write(6,*) 'OCEAN ---', na(ii), la(ii), fa(ii)  
    tmp_fa(1:nc) = fa(1:nc)
-!   sume = sum( tmp_fa(:) )
    call sratom_den(na,la,ea,tmp_fa,rpk,nc,nc+nv,it,rhoc,rho, rhoval, sf_in, &
 &              rr,vfull,zz,mmax,iexc,etot,ierr,srel)
    call voutHartree(rho,vo,rr,mmax,sume)
 
    tmp_fa(ii) = tmp_fa(ii)-1.0_DP
-!   sume = sum( tmp_fa(:) )
    call sratom_den(na,la,ea,tmp_fa,rpk,nc,nc+nv,it,rhoc,rho, rhoval, sf_in, &
 &              rr,vfull,zz,mmax,iexc,etot2,ierr,srel)
 
@@ -91,12 +99,14 @@
    
    write(6,*) 'OCEAN ---', etot, etot2, it
 
-   write(filename, '(A,I2.2)' ) 'test', ii
-   open(file=filename, unit=99, form='formatted' )
-   do jj = 1, mmax
-    write(99,'(4E24.12)') rr(jj), vo(jj), vo2(jj), rho(jj)
-   enddo
-   close(99)
+!#if DEBUG
+!   write(filename, '(A,I2.2)' ) 'test', ii
+!   open(file=filename, unit=99, form='formatted' )
+!   do jj = 1, mmax
+!    write(99,'(4E24.12)') rr(jj), vo(jj), vo2(jj), rho(jj)
+!   enddo
+!   close(99)
+!#endif
 
    write(filename, '(A8,I3.3,A1,I2.2,A1,I2.2)' ) 'vc_barez', int(zz), 'n', na(ii), 'l', la(ii )
     open(file=filename, unit=99, form='formatted' )
